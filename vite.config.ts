0bace0ff44c3ede31bfbb846edfd3608795c9ec5
Mamdouh <mamdouh.mohammed919@gmail.com>
2025-02-26 00:53:41 +0200

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "meditech", // Change output folder from 'dist' to 'build'
+    minify: "terser", // Ensures minification
+    sourcemap: false, // Disable source maps in production
+  },
 })
